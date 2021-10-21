66a8bec23444bf234906b6576dfa2253487c5c8f
Hein <h.h.l.huijskes@student.utwente.nl>
2021-10-21 12:49:37 +0200

    Tidy up some code and add comments

--- src/GUI/board.py
+++ src/GUI/board.py
@@ -1,55 +1,48 @@
-import pygame, pygame_gui, GUI_config as cf
+import pygame, config
 
-# import time # TO BE REMOVED
-
-bg = cf.bg
+bg = config.bg
 ww = bg.get_width()     # get image width
 wh = bg.get_height()    # get image height
 pieces = []
-for piece in cf.pieces:
-    pieces.append(pygame.image.load("" + cf.image_dir + "Black_" + piece + ".png"))
-    pieces.append(pygame.image.load("" + cf.image_dir + "White_" + piece + ".png"))
+for piece in config.pieces:
+    pieces.append(pygame.image.load("" + config.image_dir + "Black_" + piece + ".png"))
+    pieces.append(pygame.image.load("" + config.image_dir + "White_" + piece + ".png"))
+
 
 def draw_board(FEN, window):
     window.blit(bg, (0, 0))
-    i = 0           # the rows of the chessboard
-    index = 0       # keep track of where in the FEN-string you are
+    i = 0           # i tracks the rows of the chessboard
+    index = 0       # index points to a location in the FEN-string
     while i < 8:
-        j = 0       # the columns of the chessboard
+        j = 0       # j tracks the columns of the chessboard
         while j < 8:
-            x = FEN[index]
+            x = FEN[index]      # select the current character from the FEN-string
             index += 1
-            if x == '/':
+            if x == '/':        # end of the row on the board, continue
                 continue
-            elif x.isdigit():
-                j+=int(x)
+            elif x.isdigit():   # if x is a number, skip x columns and continue
+                j += int(x)
                 continue
             else:
-                draw_piece(x, ([j, i]), window)
-            j+=1
+                draw_piece(x, ([j, i]), window)  # reverse order of i and j since y = j = row, and x = i = column
+            j += 1
         i += 1
-    # time.sleep(5)
 
 
 def draw_piece(char, location, window):
-    color = "Black_"
-    type = 0                # type is used to find the correct piece in the pieces[] array
+    index = 0               # type is used to find the correct piece in the pieces array
     if char in "RNBQKP":
-        type += 1           # white pieces are on uneven numbers, black on even
+        index += 1          # white pieces are on uneven numbers, black on even
         char = char.lower()
     if char == "k":
-        type += 2
+        index += 2
     elif char == "n":       # knight is officially referred to as 'n' in FEN since king is already 'k'
-        type += 4
+        index += 4
     elif char == "p":
-        type += 6
+        index += 6
     elif char == "q":
-        type += 8
+        index += 8
     elif char == "r":
-        type += 10
-    # print(char)
-    # print(piece_name + " " + str(location[0]) + "," + str(location[1]))
-    # piece_name = cf.image_dir + color + type + ".png"
-    # piece = pygame.image.load(piece_name)
-    piece = pieces[type]
-    window.blit(piece, (location[0] / 8 * ww, location[1] / 8 * wh))
+        index += 10
+    chess_piece = pieces[index]
+    window.blit(chess_piece, (location[0] / 8 * ww, location[1] / 8 * wh))  # scale with window size
--- src/GUI/config.py
+++ src/GUI/config.py
@@ -4,3 +4,4 @@ START_FEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"
 image_dir = "images/"
 pieces = ["Bishop", "King", "Knight", "Pawn", "Queen", "Rook"]
 bg = pygame.image.load("images/chessboard.png")
+home_size = (800, 600)
