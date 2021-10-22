89726602c12ac4df622ea0e4d005fc3950fd82f5
Hein <h.h.l.huijskes@student.utwente.nl>
2021-10-22 11:50:07 +0200

    Fix scaling, add new images and add some comments

--- src/GUI/board.py
+++ src/GUI/board.py
@@ -1,8 +1,14 @@
-import pygame, config
+import pygame
+import config
 
 bg = config.bg
 ww = bg.get_width()     # get image width
 wh = bg.get_height()    # get image height
+offset = 0
+if config.use_offset:
+    ww = ww - config.bg_offset * 2
+    wh = wh - config.bg_offset * 2
+    offset = config.bg_offset
 pieces = []
 for piece in config.pieces:
     pieces.append(pygame.image.load("" + config.image_dir + "Black_" + piece + ".png"))
@@ -30,13 +36,13 @@ def draw_board(FEN, window):
 
 
 def draw_piece(char, location, window):
-    index = 0               # type is used to find the correct piece in the pieces array
-    if char in "RNBQKP":
-        index += 1          # white pieces are on uneven numbers, black on even
+    index = 0               # index is used to find the correct piece in the pieces array
+    if char in "RNBQKP":    # white pieces are represented as uppercase letters
+        index += 1          # here white pieces are on uneven numbers, black on even
         char = char.lower()
     if char == "k":
         index += 2
-    elif char == "n":       # knight is officially referred to as 'n' in FEN since king is already 'k'
+    elif char == "n":       # the knight is officially referred to as 'n' in FEN since king is already 'k'
         index += 4
     elif char == "p":
         index += 6
@@ -44,5 +50,17 @@ def draw_piece(char, location, window):
         index += 8
     elif char == "r":
         index += 10
+    place_piece(index, location, window)
+
+
+def place_piece(index, location, window):
+    # The game expects square pieces and a square board
+    # It takes into account offset from numbering/letters on the side of the board, as well as the size of the board
+    # It then scales everything accordingly
+    # This should also allow for selecting different backgrounds or chess piece sprites on the fly
     chess_piece = pieces[index]
-    window.blit(chess_piece, (location[0] / 8 * ww, location[1] / 8 * wh))  # scale with window size
+    scale_size = (ww / 8, ww / 8)   # scale according to the size of the board
+    chess_piece = pygame.transform.scale(chess_piece, scale_size)
+    place = (location[0] / 8 * ww + offset, location[1] / 8 * wh + offset)
+    window.blit(chess_piece, place)  # scale with window size
+
